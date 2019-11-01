abc5100ebff3cdf2e86944ff6d4a450e3538b360
smartinrubio <smartinrubio@hotels.com>
2019-11-01 09:49:05 +0000

    Replace web.xml with annotations

---
+++ src/main/java/com/sergiomartinrubio/javaservletfilters/UppercaseServlet.java
@@ -0,0 +1,20 @@
+package com.sergiomartinrubio.javaservletfilters;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/uppercase", name = "uppercaseServlet")
+public class UppercaseServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String input = request.getParameter("input").toUpperCase();
+        PrintWriter out = response.getWriter();
+        out.println(input);
+    }
+
+}

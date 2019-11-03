753462815c399f35b9da9ad178f38636e42b95b0
Sergio Martin Rubio <econsergio@gmail.com>
2019-11-03 21:29:35 +0000

    Add binary implementation

--- src/main/java/com/sergiomartinrubio/javaservletfilters/BinaryConversionStrategy.java
+++ src/main/java/com/sergiomartinrubio/javaservletfilters/BinaryConversionStrategy.java
@@ -1,8 +1,18 @@
 package com.sergiomartinrubio.javaservletfilters;
 
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class BinaryConversionStrategy implements ConversionStrategy {
     @Override
     public String convert(String ipAddress) {
-        return null;
+        byte[] bytes;
+        try {
+            bytes = InetAddress.getByName(ipAddress).getAddress();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException();
+        }
+        return new BigInteger(1, bytes).toString(2);
     }
 }
